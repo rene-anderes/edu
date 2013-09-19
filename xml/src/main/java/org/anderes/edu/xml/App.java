850108fc10fb6dd4bcb4d71446943f1521cfacb6
René Anderes <randeres@yahoo.de>
2013-09-19 17:36:46 +0200

    Initial: oo.basics

---
+++ xml/src/main/java/org/anderes/edu/xml/App.java
@@ -0,0 +1,13 @@
+package org.anderes.edu.xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
