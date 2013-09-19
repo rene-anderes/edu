b98f040e0cc328d4942e2c536b05ff2138b6336e
René Anderes <randeres@yahoo.de>
2013-09-19 17:36:46 +0200

    Initial: XML

---
+++ xml/src/test/java/org/anderes/edu/xml/AppTest.java
@@ -0,0 +1,18 @@
+package org.anderes.edu.xml;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
