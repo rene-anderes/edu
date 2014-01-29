aa096e7e1119e796571ddf17a944ff22a8d9948c
René Anderes <randeres@yahoo.de>
2014-01-29 08:41:22 +0100

    Mave-Projekt erweitert

--- maven.database/src/test/java/org/anderes/edu/maven/database/AppTest.java
+++ maven.database/src/test/java/org/anderes/edu/maven/database/AppTest.java
@@ -1,38 +1,19 @@
 package org.anderes.edu.maven.database;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
 }
