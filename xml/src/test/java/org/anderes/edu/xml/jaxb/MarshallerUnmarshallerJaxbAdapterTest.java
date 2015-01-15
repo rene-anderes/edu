e3305db327b3d2a9992dff3ed951a3255a49f6eb
René Anderes <randeres@yahoo.de>
2015-01-15 07:40:35 +0100

    JAXB Beispiel erweitert

--- xml/src/test/java/org/anderes/edu/xml/jaxb/MarshallerUnmarshallerJaxbAdapterTest.java
+++ xml/src/test/java/org/anderes/edu/xml/jaxb/MarshallerUnmarshallerJaxbAdapterTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.Month;
@@ -18,69 +17,73 @@ import javax.xml.bind.Unmarshaller;
 
 import org.anderes.edu.xml.jaxb.adapter.generated.ObjectFactory;
 import org.anderes.edu.xml.jaxb.adapter.generated.Person;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Beispiel in dem ein XML File mittels JAXB
- * serialisiert und deserialisiert wird
+ * Beispiel in dem ein Adapter eingesetzt wird um das Datums-Objekt
+ * zu konvertieren (beim Serialisieren und deserialisieren)
  * 
  * @author René Anderes
  *
  */
 public class MarshallerUnmarshallerJaxbAdapterTest {
 
-	@Test
-	@Ignore
-	public void marshaller() {
-		
-	
+    private Unmarshaller jaxbUnmarshaller;
+    private Marshaller jaxbMarshaller;
+
+    @Before
+    public void setup() {
         try {
- 
-            final File file = File.createTempFile("person", "xml");
             final JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
-            final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-            // output pretty printed
+            jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            // writing to a file
-//            jaxbMarshaller.marshal(contactlist, file);
-//            // writing to console
-//            jaxbMarshaller.marshal(contactlist, System.out);
-//            
-            assertThat(file.exists(), is(true));
-            assertThat(file.length() > 200L, is(true));
- 
         } catch (JAXBException e) {
             fail(e.getMessage());
-        } catch (IOException e) {
-        	fail(e.getMessage());
         }
- 
-	}
-	
-	@Test
-	public void unmarshaller() {
-		try {
-			
-			// XML-File
-			final InputStream is = getClass().getResourceAsStream("/org/anderes/edu/xml/jaxb/person.xml");
-			assertThat(is, is(notNullValue()));
+    }
 
-            final JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
-            final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            
-            final Person person = (Person) jaxbUnmarshaller.unmarshal(is);
-            
-            assertThat(person, is(notNullValue()));
-            assertThat(person.getBirthday(), is(february(22, 1967)));
- 
-          } catch (JAXBException e) {
-              e.printStackTrace();
-        	  fail(e.getMessage());
-          }
-	}
-	
-	private LocalDate february(int day, int year) {
-	    return LocalDate.of(year, Month.FEBRUARY, day);
-	}
+    @Test
+    public void marshaller() throws Exception {
+        
+        // given
+        final File file = File.createTempFile("person", "xml");
+        final Person person = createSampleData();
+        
+        // when
+        jaxbMarshaller.marshal(person, file);
+        jaxbMarshaller.marshal(person, System.out);
+
+        // then
+        assertThat(file.exists(), is(true));
+        assertThat(file.length() > 200L, is(true));
+
+    }
+
+    @Test
+    public void unmarshaller() throws Exception {
+
+        // given
+        final InputStream is = getClass().getResourceAsStream("/org/anderes/edu/xml/jaxb/person.xml");
+        assertThat(is, is(notNullValue()));
+
+        // when
+        final Person person = (Person) jaxbUnmarshaller.unmarshal(is);
+
+        // than
+        assertThat(person, is(notNullValue()));
+        assertThat(person.getBirthday(), is(february(22, 1967)));
+    }
+
+    private LocalDate february(int day, int year) {
+        return LocalDate.of(year, Month.FEBRUARY, day);
+    }
+
+    private Person createSampleData() {
+        final Person person = new Person();
+        person.setName("Claude Fischer");
+        person.setBirthday(february(29, 2004));
+        return person;
+    }
 }
