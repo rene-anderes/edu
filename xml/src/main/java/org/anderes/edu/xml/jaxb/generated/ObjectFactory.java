94c485748ee5c158db634268cb179724e6757c3e
René Anderes <randeres@yahoo.de>
2015-01-28 09:32:13 +0100

    Maven JAXB Plugin eingebaut

--- xml/src/main/java/org/anderes/edu/xml/jaxb/generated/ObjectFactory.java
+++ xml/src/main/java/org/anderes/edu/xml/jaxb/generated/ObjectFactory.java
@@ -1,8 +1,8 @@
 //
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2015.01.14 um 12:13:05 PM CET 
+// Generiert: 2015.01.28 um 09:19:14 AM CET 
 //
 
 
