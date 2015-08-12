4fad6d15020db99fc8e90057ff09ecdc60b1ccfd
René Anderes <randeres@yahoo.de>
2015-08-12 11:52:06 +0200

    Kommentar / Doku hinzu gefügt

--- jpa-spring/src/test/java/org/anderes/edu/jpa/rules/DbUnitRule.java
+++ jpa-spring/src/test/java/org/anderes/edu/jpa/rules/DbUnitRule.java
@@ -44,6 +44,38 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * Diese JUnit-Rule bietet die Möglichkeit mittels Annotations die für den
+ * Test notwendigen DB-Files (in den von DBUnit unterstützten Formaten)
+ * anzugeben:<br>
+ * <p><code>@UsingDataSet(value = { "/prepaire.xls" })</code></p>
+ * Es können auch mehrere Files angegeben werden:
+ * <p><code>@UsingDataSet(value = { "/Person.xls", "Offer.xls" })</code></p>
+ * </p>
+ * Ebenfalls können das oder die Files angegeben werden mittels denen DBUnit
+ * die Datenbank nach der Test-Methode überprüft:
+ * <p><code>@ShouldMatchDataSet(<br>
+ * &nbsp;&nbsp; value = { "/expectedAfterDelete.xls" },<br>
+ * &nbsp;&nbsp; excludeColumns = { "INGREDIENT.ID" },<br>
+ * &nbsp;&nbsp; orderBy = { "RECIPE.UUID", "INGREDIENT.ID" })</code>
+ * </p>
+ * Wie das Beispiel zeigt können Sortierung und Ausnahmen für den Vergleich
+ * definiert werden. Format: [Tabellenname].[Spaltennamen]
+ * <p>
+ * Wenn gwünscht, kann auch ein SQL-Script angegeben werden, mittels dem die Tabelle(n)
+ * vor der Test-Methode gelöscht werden.
+ * <p><code>@CleanupUsingScript(value = { "/sql/DeleteTableContentScript.sql" })</code></p>
+ * <p>
+ * Es wird im Klassenpfad nach den entsprechenden Files gesucht.<br>
+ * Als Basis für diese JUnit-Rule dient DBUnit ({@link siehe http://dbunit.sourceforge.net/})
+ * <p>
+ * Die Annotierungen <code>@UsingDataSet</code> und <code>@CleanupUsingScript</code> können
+ * sowohl für eine Methode als auch für eine Klasse gesetzt werden. Werden die Annotierungen
+ * auf der Klasse angebracht, so wird für jede Test-Methode die entsprechenden Files verwendet.
+ * 
+ * @author René Anderes
+ *
+ */
 public class DbUnitRule implements TestRule {
 
     @Retention(RUNTIME)
