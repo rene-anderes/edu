9daa9f7d7afb9c209554af76475b3fb3bb57dc2c
René Anderes <randeres@yahoo.de>
2015-09-13 10:34:28 +0200

    kleinere Anpassungen

--- cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/HelloWorldResource.java
+++ cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/HelloWorldResource.java
@@ -2,8 +2,6 @@ package org.anderes.edu.appengine.cookbook;
 
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
-import java.util.logging.Logger;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -13,16 +11,16 @@ import javax.ws.rs.Produces;
 @Path("/helloworld")
 public class HelloWorldResource {
 
-    private Logger logger = Logger.getLogger(this.getClass().getName());
+//    private Logger logger = Logger.getLogger(this.getClass().getName());
    	
 	@PUT
     @Consumes(TEXT_PLAIN)
     public void putMessage(String message) {
-        logger.info("Meldung (text/plain): " + message);
+//        logger.info("Meldung (text/plain): " + message);
     }
 
 	@GET
-	@Produces({TEXT_PLAIN})
+	@Produces(TEXT_PLAIN)
 	public String getClichedMessage() {
 	    return "Hello World";
 	}
