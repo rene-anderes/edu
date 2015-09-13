3449055b3e36a7ba086c2a3381b83f020b0b1716
René Anderes <randeres@yahoo.de>
2015-09-13 10:47:43 +0200

    ClodCookbook works

--- cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/HelloWorldResource.java
+++ cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/HelloWorldResource.java
@@ -2,6 +2,8 @@ package org.anderes.edu.appengine.cookbook;
 
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -11,12 +13,12 @@ import javax.ws.rs.Produces;
 @Path("/helloworld")
 public class HelloWorldResource {
 
-//    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Logger logger = Logger.getLogger(this.getClass().getName());
    	
 	@PUT
     @Consumes(TEXT_PLAIN)
     public void putMessage(String message) {
-//        logger.info("Meldung (text/plain): " + message);
+        logger.info("Meldung (text/plain): " + message);
     }
 
 	@GET
