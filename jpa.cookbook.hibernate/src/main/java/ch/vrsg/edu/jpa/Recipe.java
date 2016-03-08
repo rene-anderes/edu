15edb6ca9cdc43fb8250e33f59e63e96a0870775
René Anderes <randeres@yahoo.de>
2016-03-08 07:43:41 +0100

    JPA-ENtities ausgebaut, log richtig konfiguriert

--- jpa.cookbook.hibernate/src/main/java/ch/vrsg/edu/jpa/Recipe.java
+++ jpa.cookbook.hibernate/src/main/java/ch/vrsg/edu/jpa/Recipe.java
@@ -11,12 +11,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -36,25 +39,26 @@ public class Recipe {
 
     @SuppressWarnings("unused")
     private static final long serialVersionUID = 1L;
-
+  
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Version
     private Timestamp lastUpdate;
-
     @Column(nullable = false, length = 200)
     private String title;
-
     @Embedded
     @Column(nullable = true)
     private Image image;
-    
     @Temporal(TemporalType.DATE)
-    private Date modifyDate = new Date();
+    private Date addingDate;
+    private Integer rating;
+    @ElementCollection
+    @CollectionTable(name="TAGS", joinColumns=@JoinColumn(name="RECIPE_ID"))
+    private Set<String> tags = new HashSet<String>();
 
     @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    @JoinColumn(name = "RECIPE_ID")
     private Set<Ingredient> ingredients = new HashSet<Ingredient>();
 
     public Image getImage() {
@@ -81,11 +85,36 @@ public class Recipe {
         this.title = title;
     }
 
+    public void setAddingDate(Date addingDate) {
+        this.addingDate = addingDate;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Recipe addTag(final String tag) {
+        this.tags.add(tag);
+        return this;
+    }
+    
+    public void removeTag(final String tag) {
+        this.tags.remove(tag);
+    }
+    
     @Override
     public String toString() {
         return new StrBuilder().append("Title: " + title).appendPadding(1, ' ')
-                        .append("Image: " + image).appendPadding(1, ' ')
-                        .appendAll("Ingredients: " + ingredients).toString();
+                        .append("Rating: " + rating).appendPadding(1, ' ')
+                        .append("AddingDate: " + addingDate).appendPadding(1, ' ')
+                        .append(image).appendPadding(1, ' ')
+                        .appendAll("Ingredients: " + ingredients).appendPadding(1, ' ')
+                        .appendAll("Tags: " + tags)
+                        .toString();
     }
 
     public void addIngredient(final Ingredient ingredient) {
@@ -98,7 +127,8 @@ public class Recipe {
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(title).append(image).append(ingredients.toArray()).toHashCode();
+        return new HashCodeBuilder(17, 37).append(title).append(image).append(rating)
+                        .append(addingDate).append(tags.toArray()).append(ingredients.toArray()).toHashCode();
     }
 
     @Override
@@ -113,6 +143,8 @@ public class Recipe {
             return false;
         }
         Recipe rhs = (Recipe) obj;
-        return new EqualsBuilder().append(title, rhs.title).append(image, rhs.image).append(ingredients.toArray(), rhs.ingredients.toArray()).isEquals();
+        return new EqualsBuilder().append(title, rhs.title).append(image, rhs.image)
+                        .append(rating, rhs.rating).append(tags.toArray(), rhs.tags.toArray())
+                        .append(ingredients.toArray(), rhs.ingredients.toArray()).isEquals();
     }
 }
--- jpa.cookbook.hibernate/src/test/java/ch/vrsg/edu/jpa/RecipeTest.java
+++ jpa.cookbook.hibernate/src/test/java/ch/vrsg/edu/jpa/RecipeTest.java
@@ -11,6 +11,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -24,25 +25,18 @@ import javax.persistence.criteria.Root;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RecipeTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass().getName());
-    private EntityManagerFactory emf;
+    private static EntityManagerFactory emf;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setupOnce() throws Exception {
         emf = Persistence.createEntityManagerFactory("Cookbook");
     }
-
-    @After
-    public void tearDown() throws Exception {
- 
-    }
-
 
     /**
      * Hinzufügen eines neuen Rezepts
@@ -58,7 +52,6 @@ public class RecipeTest {
         assertThat(fromDb, is(not(sameInstance(newData))));
 
         logger.info("\n--- " + readEntity(Recipe.class, id) + " ---\n");
-
     }
 
     /**
@@ -79,8 +72,9 @@ public class RecipeTest {
      */
     @SuppressWarnings("boxing")
     private void checkRecipeWithNamedQuery() {
-        EntityManager em = emf.createEntityManager();
-        TypedQuery<Recipe> tQuery = em.createNamedQuery(Recipe.RECIPE_QUERY_ALL, Recipe.class);
+        final EntityManager em = emf.createEntityManager();
+        final TypedQuery<Recipe> tQuery = em.createNamedQuery(Recipe.RECIPE_QUERY_ALL, Recipe.class);
+        
         assertThat(tQuery.getResultList().size(), is(2));
         for (Recipe r : tQuery.getResultList()) {
             logger.info("Rezept: " + r.toString());
@@ -95,7 +89,8 @@ public class RecipeTest {
      */
     @SuppressWarnings("boxing")
     private void checkRecipeWithCriteriaAPI() {
-        List<Recipe> list = selectAllRecipeWithCriteriaAPI();
+        final List<Recipe> list = selectAllRecipeWithCriteriaAPI();
+        
         assertThat(list.size(), is(2));
         for (Recipe r : list) {
             logger.info(r.toString());
@@ -110,7 +105,7 @@ public class RecipeTest {
      */
     @SuppressWarnings("boxing")
     private void checkIngredientWithCriteriaAPI() {
-        List<Ingredient> list = selectAllIngredientWithCriteria();
+        final List<Ingredient> list = selectAllIngredientWithCriteria();
         assertThat(list.size(), is(4));
     }
 
@@ -121,14 +116,14 @@ public class RecipeTest {
      * @return Liste aller Rezepte
      */
     private List<Recipe> selectAllRecipeWithCriteriaAPI() {
-        EntityManager em = emf.createEntityManager();
+        final EntityManager em = emf.createEntityManager();
 
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Recipe> query = cb.createQuery(Recipe.class);
-        Root<Recipe> recipe = query.from(Recipe.class);
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery<Recipe> query = cb.createQuery(Recipe.class);
+        final Root<Recipe> recipe = query.from(Recipe.class);
         query.select(recipe);
-        TypedQuery<Recipe> typedQuery = em.createQuery(query);
-        List<Recipe> recipes = typedQuery.getResultList();
+        final TypedQuery<Recipe> typedQuery = em.createQuery(query);
+        final List<Recipe> recipes = typedQuery.getResultList();
 
         assertThat(recipes, is(notNullValue()));
 
@@ -136,21 +131,20 @@ public class RecipeTest {
     }
 
     /**
-     * Liefert eine Liste mit allen Zutaten zur�ck. Mittels. Mittels <b>Criteria
-     * API</b><br>
+     * Liefert eine Liste mit allen Zutaten zurück. Mittels. Mittels <b>Criteria API</b><br>
      * Entspricht JPQL "Select r from Ingredient r"
      * 
      * @return Liste mit allen Zutaten
      */
     private List<Ingredient> selectAllIngredientWithCriteria() {
-        EntityManager em = emf.createEntityManager();
+        final EntityManager em = emf.createEntityManager();
 
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Ingredient> query = cb.createQuery(Ingredient.class);
-        Root<Ingredient> ingredient = query.from(Ingredient.class);
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery<Ingredient> query = cb.createQuery(Ingredient.class);
+        final Root<Ingredient> ingredient = query.from(Ingredient.class);
         query.select(ingredient);
-        TypedQuery<Ingredient> typedQuery = em.createQuery(query);
-        List<Ingredient> ingredients = typedQuery.getResultList();
+        final TypedQuery<Ingredient> typedQuery = em.createQuery(query);
+        final List<Ingredient> ingredients = typedQuery.getResultList();
 
         assertThat(ingredients, is(notNullValue()));
 
@@ -158,8 +152,8 @@ public class RecipeTest {
     }
 
     private <T> void createEntity(T entity) {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
+        final EntityManager em = emf.createEntityManager();
+        final EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
             em.persist(entity);
@@ -178,7 +172,7 @@ public class RecipeTest {
     }
 
     private enum RecipeTestData {
-        DATA1("Rezept für Pasta"), DATA2("Anderes rezept für Pasta");
+        DATA1("Rezept für Pasta"), DATA2("Anderes Rezept für Pasta");
 
         private String title;
 
@@ -188,6 +182,9 @@ public class RecipeTest {
         public Recipe createRecipe() {
             Recipe newData = new Recipe();
             newData.setTitle(this.title);
+            newData.setAddingDate(new Date());
+            newData.setRating(4);
+            newData.addTag("pasta").addTag("vegetarisch");
             newData.setImage(new Image("/images/test.jpg", "Bild für URL"));
             newData.addIngredient(new Ingredient("200g", "Pasta", "aus der Migros"));
             newData.addIngredient(new Ingredient("200g", "Pelati", "besser als frische Tomaten"));
