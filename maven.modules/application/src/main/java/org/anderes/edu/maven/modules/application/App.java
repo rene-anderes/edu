dc4b623a74a84e906ceaea1ddcfe626828a342c6
René Anderes <randeres@yahoo.de>
2016-04-26 11:08:37 +0200

    Mavne Beispiel mit Multi-Modul-Projekt und Assembly-Plugin

--- maven.modules/application/src/main/java/org/anderes/edu/maven/modules/application/App.java
+++ maven.modules/application/src/main/java/org/anderes/edu/maven/modules/application/App.java
@@ -4,10 +4,8 @@ package org.anderes.edu.maven.modules.application;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
