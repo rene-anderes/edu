c9acc091185c4e760adf208521876e1d26d30bad
René Anderes <randeres@yahoo.de>
2016-06-08 13:16:54 +0200

    Imports angepasst

--- oo.basics/src/test/java/org/anderes/edu/testing/junit/sample01/MyFirstJUnitTest.java
+++ oo.basics/src/test/java/org/anderes/edu/testing/junit/sample01/MyFirstJUnitTest.java
@@ -1,7 +1,9 @@
 package org.anderes.edu.testing.junit.sample01;
 
-import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
