02a2f0c73c5b13655526021eb677a3f3b5876af6
René Anderes <randeres@yahoo.de>
2016-07-05 14:48:52 +0200

    JAX-RS Sample: CacheControl

--- jee.rest.sample/src/main/java/org/anderes/edu/jee/rest/sample/UserResource.java
+++ jee.rest.sample/src/main/java/org/anderes/edu/jee/rest/sample/UserResource.java
@@ -1,19 +1,46 @@
 package org.anderes.edu.jee.rest.sample;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
-import static javax.ws.rs.core.MediaType.*;
 
-@Path("/users/{username}")
+@Path("users")
 public class UserResource {
 
 	@GET
+	@Path("{username}")
 	@Produces(TEXT_PLAIN)
 	public Response getUser(@PathParam("username") String userName) {
-		return Response.ok().entity(userName).build();
+	    final CacheControl cacheControl = new CacheControl();
+        cacheControl.setMaxAge(5);
+        cacheControl.setPrivate(true);
+		return Response.ok().entity(userName).cacheControl(cacheControl).build();
 	}
 
+	/**
+	 * Wird für das Mapping der {@code List<String>} MOXy verwendet, wird ein Fehler 500
+	 * auf dem Server ausgelöst: 
+	 * "MessageBodyWriter not found for media type=application/json, 
+	 *  type=class java.util.ArrayList, genericType=java.util.List<java.lang.String>."<br>
+	 *  Das ist ein MOXy-Problem und tritt mit z.B. Jackson nicht auf.
+	 */
+	@GET
+	@Produces(APPLICATION_JSON)
+	public Response getAllUser() {
+	    final List<String> userlist = new ArrayList<String>(Arrays.asList("Bill", "Tom", "Bob"));
+	    final GenericEntity<List<String>> entity = new GenericEntity<List<String>>(userlist) {};
+        return Response.ok().encoding(UTF_8.name()).entity(entity).build();
+	}
 }
---
+++ jee.rest.sample/src/test/java/org/anderes/edu/jee/sample/UserResourceTest.java
@@ -0,0 +1,69 @@
+package org.anderes.edu.jee.sample;
+
+import static javax.ws.rs.core.MediaType.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.anderes.edu.jee.rest.sample.UserResource;
+import org.glassfish.jersey.CommonProperties;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.Test;
+
+public class UserResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        return new ResourceConfig(UserResource.class).property(CommonProperties.MOXY_JSON_FEATURE_DISABLE_SERVER, Boolean.TRUE);
+        /** MOXY und List<String> funktioniert nicht (BUG) -> Disable MOXY */
+    }
+    
+    @Test
+    public void shouldBeFindAllUser() {
+        final Response response = target("users").request(APPLICATION_JSON_TYPE).buildGet().invoke();
+       
+        assertThat(response, is(notNullValue()));
+        assertThat(response.getStatusInfo(), is(Status.OK));
+        assertThat(response.hasEntity(), is(true));
+        final List<String> users = response.readEntity(new GenericType<List<String>>() {});
+        assertThat(users, is(notNullValue()));
+        assertThat(users.size(), is(3));
+    }
+    
+    @Test
+    public void shouldBeCorrectHeader() {
+        final Response response = target("users/john").request(TEXT_PLAIN_TYPE).buildGet().invoke();
+        
+        assertThat(response, is(notNullValue()));
+        assertThat(response.getStatusInfo(), is(Status.OK));
+        
+        // Cache-Control
+        final Map<String, List<Object>> headers = response.getHeaders();
+        assertThat(headers.containsKey("Cache-Control"), is(true));
+        final List<Object> cache_control_header = headers.get("Cache-Control");
+        assertThat(cache_control_header.size(), is(1));
+        final String cache_control = (String) cache_control_header.get(0);
+        assertThat(cache_control.contains("max-age=5"), is(true));
+        assertThat(cache_control.contains("no-store"), is(false));
+        assertThat(cache_control.contains("private"), is(true));
+        
+        assertThat(response.hasEntity(), is(true));
+        final String users = response.readEntity(String.class);
+        assertThat(users, is(notNullValue()));
+        assertThat(users, is("john"));
+    }
+}
