18cf9a3421f16c2636a003d584766f853a7a1499
René Anderes <randeres@yahoo.de>
2016-10-19 11:31:44 +0200

    CDI Beispiel angepasst, damit sie mit der Schulung überein stimmt

--- jee.rest.cdi.advanced/src/main/java/org/anderes/edu/jee/cdi/advanced/RelaxedGreetingService.java
+++ jee.rest.cdi.advanced/src/main/java/org/anderes/edu/jee/cdi/advanced/RelaxedGreetingService.java
@@ -1,8 +1,9 @@
 package org.anderes.edu.jee.cdi.advanced;
 
 @Relaxed
-public class RelaxedGreetingService {
+public class RelaxedGreetingService implements GreetingService {
 
+    @Override
     public String sayHello() {
         return "Dear World";
     }
