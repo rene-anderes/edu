52a4db03b84ccd0c4c55b9829b8e6de43ed785d8
René Anderes <randeres@yahoo.de>
2016-12-28 14:49:58 +0100

    GAE Kochbuch angepasst

--- cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/rest/MyApplication.java
+++ cloudcookbook/src/main/java/org/anderes/edu/appengine/cookbook/rest/MyApplication.java
@@ -12,9 +12,7 @@ public class MyApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         final Set<Class<?>> clazz = new HashSet<Class<?>>();
-        clazz.add(HelloWorldResource.class);
         clazz.add(RecipeResource.class);
-        clazz.add(UserAuthResources.class);
         clazz.add(NotFoundExceptionMapper.class);
         return clazz;
     }
