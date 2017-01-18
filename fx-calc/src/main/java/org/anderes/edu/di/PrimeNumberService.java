6c675af7b345cbb3bc3cae17f83d153db369e857
René Anderes <randeres@yahoo.de>
2017-01-18 14:34:31 +0100

    UPN Taschenrechner

---
+++ fx-calc/src/main/java/org/anderes/edu/di/PrimeNumberService.java
@@ -0,0 +1,17 @@
+package org.anderes.edu.di;
+
+import static java.lang.Math.sqrt;
+
+import java.util.stream.LongStream;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class PrimeNumberService implements Service {
+
+    @Override
+    public Boolean isPrimeNumber(final Integer number) {
+        return number > 1 && LongStream.rangeClosed(2, (long)sqrt(number)).noneMatch(divisor -> number % divisor == 0);
+    }
+
+}
