35b09780ac2c0b01e64d37c2bc07e0706b731d8c
René Anderes <randeres@yahoo.de>
2017-01-21 16:22:37 +0100

    DI mit Spring angepasst

--- di-for-java/src/main/java/org/anderes/edu/di/spring/CalcConfigSpring.java
+++ di-for-java/src/main/java/org/anderes/edu/di/spring/CalcConfigSpring.java
@@ -5,6 +5,7 @@ import org.anderes.edu.di.PrimeNumberService;
 import org.anderes.edu.di.Service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class CalcConfigSpring {
@@ -14,7 +15,9 @@ public class CalcConfigSpring {
         return new PrimeNumberService();
     }
 
-    @Bean
+    /* The default scope is singleton, but you can override this with the @Scope annotation as follows: */
+    @Bean 
+    @Scope("prototype")
     public Calc calc() {
         return new Calc();
     }
