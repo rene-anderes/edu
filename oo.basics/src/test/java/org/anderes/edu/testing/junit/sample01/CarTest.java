ced890b28831c88f1aa0fd92a9a8debe6a5b2747
René Anderes <randeres@yahoo.de>
2017-06-29 14:41:25 +0200

    JUnit-Beispiele erweitert

--- oo.basics/src/test/java/org/anderes/edu/testing/junit/sample01/CarTest.java
+++ oo.basics/src/test/java/org/anderes/edu/testing/junit/sample01/CarTest.java
@@ -5,21 +5,42 @@ import static org.anderes.edu.testing.junit.sample01.DoorState.OPEN;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value = Parameterized.class)
 public class CarTest {
 
     @Rule
     public final ExpectedException thrown = ExpectedException.none();
     
     private Car carUnderTest;
+    private final Integer numberOfDoor;
     
     @Before
     public void setup() {
-        carUnderTest = new Car(4);
+        carUnderTest = new Car(numberOfDoor);
+    }
+
+    public CarTest(Integer number) {
+        numberOfDoor = number;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> collection = new ArrayList<>(3); 
+        collection.add(new Integer[] { 2 });
+        collection.add(new Integer[] { 3 });
+        collection.add(new Integer[] { 4 });
+        return collection;
     }
     
     @Test
@@ -60,7 +81,7 @@ public class CarTest {
         
         // given
         thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage(is("Die Türnummer muss grösser als 0 und nicht grösser als 4 sein."));
+        thrown.expectMessage(is("Die Türnummer muss grösser als 0 und nicht grösser als " + numberOfDoor + " sein."));
         
         // when
         carUnderTest.isDoorClose(8);
